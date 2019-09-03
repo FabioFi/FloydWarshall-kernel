e1eb87b7f56cd10051cac32a15f98c57fb611b69
Fabio Fiorio | Exor International S.p.A <fabio.fiorio@exorint.com>
2019-09-03 14:51:39 +0200

    3fw

--- kernel.cu
+++ kernel.cu
@@ -2,11 +2,14 @@
 #include "Graph/GraphWeight.hpp"
 #include <tuple>
 #include <limits>
-#include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <random>
 #include <chrono>
+
+#include <thread>
+
+#include <fstream>
 
 
 using matrix_t = float; //matrix_t is like float
@@ -19,25 +22,29 @@ using namespace std::chrono;
 //nE => numero di archi nel grafo
 
 
-const int BLOCK_SIZE_X = 16;
-const int BLOCK_SIZE_Y = 16;
+const int BLOCK_SIZE = 16;
 
-template<typename T> __global__ void floyd_warshall_kernel(T* matrix, int num_vertices, float INF, int k) {
+const float INF = std::numeric_limits<float>::infinity();
 
-    unsigned int X = blockIdx.x * blockDim.x +threadIdx.x;  //colonne
-    unsigned int Y = blockIdx.y * blockDim.y +threadIdx.y;  //righe
 
-    if(X<num_vertices && Y<num_vertices){
+template<typename T> __global__ void floyd_warshall_kernel(T* matrix, int num_vertices, int k) {
+
+    int id = blockIdx.x * blockDim.x + threadIdx.x;  //colonne
+
+    if(id<num_vertices*num_vertices){
+
+        int Y = id/num_vertices;
+        int X = id - Y*num_vertices;
+
         if (matrix[Y*num_vertices + k] != INF &&
             matrix[k*num_vertices + X] != INF &&
-            matrix[Y*num_vertices + k] + matrix[k*num_vertices + X] < matrix[Y*num_vertices + X])
-                matrix[Y*num_vertices + X] = matrix[Y*num_vertices + k] + matrix[k*num_vertices + X];
-    } 
+            matrix[Y*num_vertices + k] + matrix[k*num_vertices + X] < matrix[Y*num_vertices+X])
+                matrix[Y*num_vertices+X] = matrix[Y*num_vertices + k] + matrix[k*num_vertices + X];
+    }
 }
 
-int main(int argc, char* argv[]) {
+__host__ int main(int argc, char* argv[]) {
 
-    const float INF = std::numeric_limits<float>::infinity();
 
     // se non trova due argomenti termina perchè manca l'input
     if (argc != 2)
@@ -45,7 +52,7 @@ int main(int argc, char* argv[]) {
 
     // -------------------------------------------------------------------------
     // HOST MEMORY ALLOCATION
-    graph::GraphWeight<int, int, matrix_t> graph(graph::structure_prop::COO);   // arco da int A a int B di peso float P 
+    graph::GraphWeight<int, int, matrix_t> graph(graph::structure_prop::COO);   // arco da int A a int B di peso float P
     graph.read(argv[1]);    //si copia da file i valori dati
 
     auto matrix = new matrix_t*[graph.nV()];    //usata per la parte di codice sequenziale
@@ -70,9 +77,10 @@ int main(int argc, char* argv[]) {
         for(int j = 0; j < graph.nV(); j++)
             h_matrix[i*graph.nV() + j] = matrix[i][j];
 
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    auto t1 = std::chrono::system_clock::now();
+    //std::this_thread::sleep_for(seconds(5));
     floyd_warshall::floyd_warshall(matrix, graph.nV()); //codice sequenziale
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    auto t2 = std::chrono::system_clock::now();
     long double duration = duration_cast<milliseconds>( t2 - t1 ).count();
     std::cout << "TIME CPU " << duration/1000 << '\n';
 
@@ -89,8 +97,8 @@ int main(int argc, char* argv[]) {
     // -------------------------------------------------------------------------
     // DEVICE EXECUTION
 
-    dim3 block_size(BLOCK_SIZE_X,BLOCK_SIZE_Y);
-    dim3 num_blocks( (graph.nV()+BLOCK_SIZE_X-1)/BLOCK_SIZE_X, (graph.nV()+BLOCK_SIZE_Y-1)/BLOCK_SIZE_Y);
+    //dim3 block_size(BLOCK_SIZE_X,BLOCK_SIZE_Y);
+    //dim3 num_blocks( (graph.nV()+BLOCK_SIZE_X-1)/BLOCK_SIZE_X, (graph.nV()+BLOCK_SIZE_Y-1)/BLOCK_SIZE_Y);
 
     /// time start
     cudaEvent_t startTimeCuda, stopTimeCuda;
@@ -99,8 +107,8 @@ int main(int argc, char* argv[]) {
     cudaEventRecord(startTimeCuda, 0);
 
     for(int k = 0; k < graph.nV(); k++)
-        floyd_warshall_kernel<<< num_blocks, block_size >>>(d_matrix,graph.nV(), INF, k);
-    
+        floyd_warshall_kernel<<< (graph.nV()+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE >>>(d_matrix,graph.nV(), k);
+
     cudaEventRecord(stopTimeCuda,0);
     cudaEventSynchronize(stopTimeCuda);
     float msTime;
@@ -113,15 +121,26 @@ int main(int argc, char* argv[]) {
     // COPY DATA FROM DEVICE TO HOST
     cudaMemcpy(h_matrix, d_matrix, dim_data, cudaMemcpyDeviceToHost);
 
+    // CREATE A FILE OF VALUES
+    std::ofstream myfile;
+    myfile.open ("../data.csv");
+    for (int i = 0; i < graph.nV(); i++){
+        for(int j = 0; j < graph.nV(); j++){
+            myfile << h_matrix[i*graph.nV() + j] << ",";
+        }
+        myfile << '\n';
+    }
+    myfile.close();
+
     //--------------------------------------------------------------------------
     // RESULT CHECK
     for (int i = 0; i < graph.nV(); i++)
         for(int j = 0; j < graph.nV(); j++)
             if (h_matrix[i*graph.nV()+j] != matrix[i][j]) {
                 std::cerr << "wrong result at: ("
-                        << (i / graph.nV()) << ", " << (i % graph.nV()) << ")"
-                        << "\nhost:   " << h_matrix[i*graph.nV()+j]
-                        << "\ndevice: " << matrix[i][j] << "\n\n";
+                        << i << ", " << j << ")"
+                        << "\nhost:   " << matrix[i][j]
+                        << "\ndevice: " << h_matrix[i*graph.nV()+j] << "\n\n";
                 cudaDeviceReset();
                 std::exit(EXIT_FAILURE);
             }
