15876f0623e6a146275941c8ba7c8eacc65b74e6
Fabio Fiorio | Exor International S.p.A <fabio.fiorio@exorint.com>
2019-09-03 14:47:31 +0200

    2_branchDiv_FloydWarshall

--- include/kernel.h
+++
@@ -1,2 +0,0 @@
-void cuda_code(float* h_matrix, float** matrix, float INF, int nV, int BLOCK_SIZE, long double duration);
-
--- kernel.cu
+++ kernel.cu
@@ -6,7 +6,10 @@
 #include <iostream>
 #include <random>
 #include <chrono>
-#include <algorithm>    // std::max
+
+#include <thread>
+
+#include <fstream>
 
 
 using matrix_t = float; //matrix_t is like float
@@ -18,120 +21,39 @@ using namespace std::chrono;
 //nV => numero di vettori del grafo
 //nE => numero di archi nel grafo
 
+
 const int BLOCK_SIZE = 32;
 
-__constant__ float dINF;
-__constant__ int dnV;
-__constant__ int dBS;
+const float INF = std::numeric_limits<float>::infinity();
 
-bool cmpFloats2(float a, float b) {
-    const float abs_a = std::abs(a);
-    const float abs_b = std::abs(b);
-    const float epsilon = 0.00001f;  // prende solo 4 valori dopo la virgola
-    float diff = std::abs(abs_a - abs_b);
-    if(diff / std::max(abs_a, abs_b) > epsilon)
-        return true;
-    return false;
-}
 
-// bool ph indirizza la funzione di calcolo floyd warshall se siamo nella fase 3 o no [ph == true allora siamo nella fase 3]
-__forceinline__ __device__ void floyd_warshall_kernel(float* matrix, int Y, int X, int y_c, int x_c, int y_a, int x_a, int y_b, int x_b, bool ph3){
+template<typename T> __global__ void floyd_warshall_kernel(T* matrix, int num_vertices, int k) {
 
-    const unsigned int moveA = dnV*(y_a + Y) + x_a;
-    const unsigned int moveB = y_b*dnV + x_b + X;
+    unsigned int X = blockIdx.x * blockDim.x +threadIdx.x;  //colonne
+    unsigned int Y = blockIdx.y * blockDim.y +threadIdx.y;  //righe
 
-    const unsigned int posC = dnV*(y_c+Y) + x_c + X;
+    bool readexec;
 
-    float sum = matrix[posC];
-    float dAB;
+   __syncthreads();        
 
-    const unsigned int limity = dnV - y_b;
-    const unsigned int limitx = dnV - x_a;
+    if(X<num_vertices && Y<num_vertices){
+        if(matrix[Y * num_vertices + X] == INF)
+            matrix[Y*num_vertices + X] = matrix[Y*num_vertices + k] + matrix[k*num_vertices + X];
+        else
+            if(matrix[Y*num_vertices + k] + matrix[k*num_vertices + X] < matrix[Y*num_vertices + X])
+                matrix[Y*num_vertices + X] = matrix[Y*num_vertices + k] + matrix[k*num_vertices + X];
 
-    if(!ph3){
-        for(unsigned int k = 0; k < dBS && k < limitx && k < limity; k++){
-            dAB = matrix[moveA + k] + matrix[moveB + k*dnV];
-            if(dAB != dINF && sum > dAB){
-                sum = dAB;
-                matrix[posC] = sum;}
-            __syncthreads();
-        }
-    }
-
-    else{
-        for(unsigned int k = 0; k < dBS && k < limitx && k < limity; k++){
-            dAB = matrix[moveA + k] + matrix[moveB + k*dnV];
-            if(dAB != dINF &&  sum > dAB)
-                sum = dAB;
-        }
-        
-    matrix[posC] = sum;
     }
 }
 
-// FASE 1 => BLOCCO PRIMARIO
-__global__ void block_ph1(float* matrix, int pbid){
+/*
+for (int i = 1; i < blockDim.x; i *= 2) {
+Heavy Branch
+Divergence
+if (threadIdx.x % (i * 2) == 0)
+*/
 
-    const unsigned int by = blockIdx.y;
-    const unsigned int bx = blockIdx.x;
-    
-    const unsigned int pb_point = by*dBS;
-
-    const unsigned int Y = threadIdx.y;
-    const unsigned int X = threadIdx.x;
-
-    if(by != pbid || bx != pbid || pb_point + Y >= dnV || pb_point + X >= dnV)    // se si dovesse trovare sulla diagonale
-        return;
-
-    floyd_warshall_kernel(matrix, Y, X, pb_point, pb_point, pb_point, pb_point, pb_point, pb_point, false);
-} 
-
-// FASE 2 => BLOCCHI CON RIGHE O COLONNE IN COMUNE AL BLOCCO PRIMARIO
-__global__ void block_ph2(float* matrix, int pbid){
-
-    const unsigned int by = blockIdx.y;
-    const unsigned int bx = blockIdx.x;
-
-    const unsigned int by_point = by*dBS;
-    const unsigned int bx_point = bx*dBS;
-    const unsigned int pb_point = pbid*dBS;
-
-    if((bx == 0 && by == pbid) || (bx == pbid && by == 0))
-        return;
-
-    const unsigned int Y = threadIdx.y;
-    const unsigned int X = threadIdx.x;
-
-    if(bx == 0 && by_point + Y < dnV && pb_point + X < dnV)
-        floyd_warshall_kernel(matrix, Y, X, by_point, pb_point, by_point, pb_point, pb_point, pb_point, false); // C, C, B
-    if(by == 0 && pb_point + Y < dnV && bx_point + X < dnV)
-        floyd_warshall_kernel(matrix, Y, X, pb_point, bx_point, pb_point, pb_point, pb_point, bx_point, false); // C, A, C
-}
-
-//  FASE 3 => BLOCCHI RIMANENTI
-__global__ void block_ph3(float* matrix, int pbid){
-
-    const unsigned int by = blockIdx.y;
-    const unsigned int bx = blockIdx.x;
-
-    const unsigned int by_point = by*dBS;
-    const unsigned int bx_point = bx*dBS;
-    const unsigned int pb_point = pbid *dBS;
-
-    const unsigned int Y = threadIdx.y;
-    const unsigned int X = threadIdx.x;
-    
-    if(bx == pbid || by == pbid || bx_point + X >= dnV || by_point + Y >= dnV)
-        return;
-
-    floyd_warshall_kernel(matrix, Y, X, by_point, bx_point, by_point, pb_point, pb_point, bx_point, true);
-}
-
-// CODICE LANCIATO SULLA CPU
 __host__ int main(int argc, char* argv[]) {
-
-    int count = 0;
-        cudaGetDeviceCount(&count);
 
     // se non trova due argomenti termina perchè manca l'input
     if (argc != 2)
@@ -142,16 +64,13 @@ __host__ int main(int argc, char* argv[]) {
     graph::GraphWeight<int, int, matrix_t> graph(graph::structure_prop::COO);   // arco da int A a int B di peso float P 
     graph.read(argv[1]);    //si copia da file i valori dati
 
-    const unsigned int nV = graph.nV();
-    const float INF = std::numeric_limits<float>::infinity();
-
-    auto matrix = new matrix_t*[nV];    //usata per la parte di codice sequenziale
+    auto matrix = new matrix_t*[graph.nV()];    //usata per la parte di codice sequenziale
 
     // -------------------------------------------------------------------------
     // HOST INITILIZATION
-    for (int i = 0; i < nV; i++) {
-        matrix[i] = new matrix_t[nV];
-        std::fill(matrix[i], matrix[i] + nV, std::numeric_limits<matrix_t>::infinity());    //inizializza tutti gli archi a INF
+    for (int i = 0; i < graph.nV(); i++) {
+        matrix[i] = new matrix_t[graph.nV()];
+        std::fill(matrix[i], matrix[i] + graph.nV(), std::numeric_limits<matrix_t>::infinity());    //inizializza tutti gli archi a INF
     }
     for (int i = 0; i < graph.nE(); i++) {
         auto index = graph.coo_ptr()[i];
@@ -161,15 +80,15 @@ __host__ int main(int argc, char* argv[]) {
     //--------------------------------------------------------------------------
     // HOST EXECUTIION
 
-    float* h_matrix = new float[nV*nV];
+    float* h_matrix = new float[graph.nV()*graph.nV()];
 
-    for (int i = 0; i < nV; i++)
-        for(int j = 0; j < nV; j++)
-            if(i < nV && j < nV)
-                h_matrix[i*nV + j] = matrix[i][j];
+    for (int i = 0; i < graph.nV(); i++)
+        for(int j = 0; j < graph.nV(); j++)
+            h_matrix[i*graph.nV() + j] = matrix[i][j];
 
     auto t1 = std::chrono::system_clock::now();
-    floyd_warshall::floyd_warshall(matrix, nV); //codice sequenziale
+    //std::this_thread::sleep_for(seconds(5));
+    floyd_warshall::floyd_warshall(matrix, graph.nV()); //codice sequenziale
     auto t2 = std::chrono::system_clock::now();
     long double duration = duration_cast<milliseconds>( t2 - t1 ).count();
     std::cout << "TIME CPU " << duration/1000 << '\n';
@@ -177,56 +96,32 @@ __host__ int main(int argc, char* argv[]) {
     // -------------------------------------------------------------------------
     // DEVICE MEMORY ALLOCATION
     float *d_matrix;
-    const unsigned int dim_data = nV*nV*sizeof(float);
+    int dim_data = graph.nV()*graph.nV()*sizeof(float);
     cudaMalloc(&d_matrix, dim_data);
-
 
     // -------------------------------------------------------------------------
     // COPY DATA FROM HOST TO DEVIE
-    cudaMemcpy(d_matrix, h_matrix, dim_data, cudaMemcpyHostToDevice);
-    cudaMemcpyToSymbol(dINF, &INF, sizeof(float), 0, cudaMemcpyHostToDevice);
-    cudaMemcpyToSymbol(dnV, &nV, sizeof(int), 0, cudaMemcpyHostToDevice);
-    cudaMemcpyToSymbol(dBS, &BLOCK_SIZE, sizeof(int), 0, cudaMemcpyHostToDevice);
-
+    cudaMemcpy(d_matrix , h_matrix, dim_data, cudaMemcpyHostToDevice);
 
     // -------------------------------------------------------------------------
     // DEVICE EXECUTION
-    //const unsigned int dim_sblock = ceil((float) nV / (float) BLOCK_SIZE);
-    const unsigned int nblock =  (nV+BLOCK_SIZE-1)/BLOCK_SIZE;
-    //dim3 num_blocksph1(1,1);
-    dim3 block_size(BLOCK_SIZE,BLOCK_SIZE);
-    dim3 num_blocks(nblock, nblock);
 
+    dim3 block_size(BLOCK_SIZE,BLOCK_SIZE);
+    dim3 num_blocks( (graph.nV()+BLOCK_SIZE-1)/BLOCK_SIZE, (graph.nV()+BLOCK_SIZE-1)/BLOCK_SIZE);
 
     /// time start
     cudaEvent_t startTimeCuda, stopTimeCuda;
     cudaEventCreate(&startTimeCuda);
     cudaEventCreate(&stopTimeCuda);
-    cudaEventRecord(startTimeCuda);
+    cudaEventRecord(startTimeCuda, 0);
 
-    //cudaError_t error;
-
-    for(int primary_block = 0; primary_block < nblock; primary_block++){
-        //std::cout << "Iterazione numero " << primary_block << '\n';
-        block_ph1<<<num_blocks, block_size>>>(d_matrix, primary_block);
-        //error = cudaGetLastError();
-        //std::cout << "PHASE 1: " << error << '\n';
-        block_ph2<<<num_blocks, block_size>>>(d_matrix, primary_block);
-        //error = cudaGetLastError();
-        //std::cout << "PHASE 2: "  << error << '\n';
-        block_ph3<<<num_blocks, block_size>>>(d_matrix, primary_block);
-        //error = cudaGetLastError();
-        //std::cout << "PHASE 3: "  << error << '\n';
-    }
-
-    // TAKE KERNEL EXECUTION TIME
-    //cudaDeviceSynchronize();
-    cudaEventRecord(stopTimeCuda);
+    for(int k = 0; k < graph.nV(); k++)
+        floyd_warshall_kernel<<< num_blocks, block_size >>>(d_matrix,graph.nV(), k);
+    
+    cudaEventRecord(stopTimeCuda,0);
     cudaEventSynchronize(stopTimeCuda);
     float msTime;
     cudaEventElapsedTime(&msTime, startTimeCuda, stopTimeCuda);
-    cudaEventDestroy(startTimeCuda);
-    cudaEventDestroy(stopTimeCuda);
 
     std::cout << "TIME GPU " << msTime/1000 << '\n';
     std::cout << "SPEEDUP " << (duration/1000)/(msTime/1000) << '\n';
@@ -235,20 +130,29 @@ __host__ int main(int argc, char* argv[]) {
     // COPY DATA FROM DEVICE TO HOST
     cudaMemcpy(h_matrix, d_matrix, dim_data, cudaMemcpyDeviceToHost);
 
+    // CREATE A FILE OF VALUES
+    std::ofstream myfile;
+    myfile.open ("../data.csv");
+    for (int i = 0; i < graph.nV(); i++){
+        for(int j = 0; j < graph.nV(); j++){
+            myfile << h_matrix[i*graph.nV() + j] << ",";
+        }
+        myfile << '\n';
+    }
+    myfile.close();
 
     //--------------------------------------------------------------------------
     // RESULT CHECK
-
-    for (int i = 0; i < nV; i++)
-        for(int j = 0; j < nV; j++)
-            if( cmpFloats2(h_matrix[i*nV+j], matrix[i][j])) {
+    for (int i = 0; i < graph.nV(); i++)
+        for(int j = 0; j < graph.nV(); j++)
+            if (h_matrix[i*graph.nV()+j] != matrix[i][j]) {
                 std::cerr << "wrong result at: ("
-                    << i << ", " << j << ")"
-                    << "\nhost:   " << std::setprecision (15) << matrix[i][j]
-                    << "\ndevice: " << std::setprecision (15) << h_matrix[i*nV+j] << "\n\n";  
+                        << i << ", " << j << ")"
+                        << "\nhost:   " << matrix[i][j] 
+                        << "\ndevice: " << h_matrix[i*graph.nV()+j] << "\n\n";
                 cudaDeviceReset();
                 std::exit(EXIT_FAILURE);
-        }
+            }
 
     std::cout << "<> Correct\n\n";
 
@@ -256,7 +160,6 @@ __host__ int main(int argc, char* argv[]) {
     // HOST MEMORY DEALLOCATION
     delete[] h_matrix;
     delete[] matrix;
-    
 
     // -------------------------------------------------------------------------
     // DEVICE MEMORY DEALLOCATION
